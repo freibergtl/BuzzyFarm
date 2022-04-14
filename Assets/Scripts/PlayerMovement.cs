5ab5c24e49da00efbd5c2ff1ed905c4cf23e2cda
Charlie <charlieoscar@ymail.com>
2022-04-14 12:16:57 -0400

    Added Respawn Point if player falls below Y = -5

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    public Transform respawnPoint;
+
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+	{
+		if(transform.position.y < -10)
+		{
+			Respawn();
+		}
+	}
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isMoving = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsMoving", isMoving);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+
+       
+
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * 0.5f);
+        m_Rigidbody.MoveRotation(m_Rotation);
+       
+    }
+    void Respawn()
+	{
+		m_Rigidbody.velocity = Vector3.zero;
+		m_Rigidbody.angularVelocity = Vector3.zero;
+		m_Rigidbody.Sleep();
+		transform.position = respawnPoint.position;
+	}
+}
